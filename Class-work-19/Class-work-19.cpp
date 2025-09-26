281a12d3cb8787cf88e3bc0cc12f9d14d57d7fa9
Eugene <eugenenikitenko228@gmail.com>
2025-09-26 19:37:39 +0300

    Добалено прямокутник із заданими розмірами та цветом

--- Class-work-19/Class-work-19.cpp
+++ Class-work-19/Class-work-19.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
+
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	srand(static_cast<int>(time(0)));
+
+	//{------------------------------------------------------------------
 
 	//int year;
 
@@ -20,33 +23,88 @@ int main()
 	//	cout << "В цьому році 365 днів";
 	//}
 
-	int color, BackGround;
+//------------------------------------------------------------------}
 
-	cout << "Цвет: ";
+//{------------------------------------------------------------------
+
+	//int color, BackGround;
+
+	//cout << "Цвет(1 - Red, 2 - Green, 3 - Yellow): ";
+	//cin >> color;
+	//cout << "Фон: ";
+	//cin >> BackGround;
+
+	//if (color == 1) {
+	//	cout << "\033[31m";
+	//}else if (color == 2) {
+	//	cout << "\033[32m";
+	//}
+	//else if (color == 3) {
+	//	cout << "\033[33m";
+	//}
+
+
+	//if (BackGround == 1) {
+	//	cout << "\033[41m";
+	//}
+	//if (BackGround == 2) {
+	//	cout << "\033[42m";
+	//}
+	//if (BackGround == 3) {
+	//	cout << "\033[43m";
+	//}
+	//
+	//cout << "Hi world\033[0m";
+
+//------------------------------------------------------------------}
+
+	char simvol;
+	int sizeRow = 0, sizeCol = 0, color;
+
+	cout << "simvol: ";
+	cin >> simvol;
+
+	while (sizeRow < 2) {
+		system("cls");
+		cout << "Row: ";
+		cin >> sizeRow;
+	}
+	while (sizeCol < 2) {
+		system("cls");
+		cout << "Col: ";
+		cin >> sizeCol;
+	}
+
+	system("cls");
+	cout << "Color(1 - Red, 2 - Green, 3 - Yellow): ";
 	cin >> color;
-	cout << "Фон: ";
-	cin >> BackGround;
+	system("cls");
 
 	if (color == 1) {
 		cout << "\033[31m";
-	}else if (color == 2) {
+	}
+	else if (color == 2) {
 		cout << "\033[32m";
 	}
 	else if (color == 3) {
 		cout << "\033[33m";
 	}
 
-
-	if (BackGround == 1) {
-		cout << "\033[41m";
+	for (int i = 0; i < sizeCol; i++) {
+		for (int j = 0; j < sizeRow; j++) {
+			if (i == 0 || i == sizeCol-1) {
+				cout << simvol;
+			}
+			else if (j == 0 || j == sizeRow-1) {
+				cout << simvol;
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << endl;
 	}
-	if (BackGround == 2) {
-		cout << "\033[42m";
-	}
-	if (BackGround == 3) {
-		cout << "\033[43m";
-	}
-
+	cout << "\033[0m";
 
 
 
