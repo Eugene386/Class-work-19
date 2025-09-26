506dfdc8872742f3dc265ba80f894c7e03c99c9c
Eugene <eugenenikitenko228@gmail.com>
2025-09-26 19:47:45 +0300

    Додалено знаходження максимального числа в масиве

--- Class-work-19/Class-work-19.cpp
+++ Class-work-19/Class-work-19.cpp
@@ -58,54 +58,83 @@ int main()
 
 //------------------------------------------------------------------}
 
-	char simvol;
-	int sizeRow = 0, sizeCol = 0, color;
 
-	cout << "simvol: ";
-	cin >> simvol;
+//{------------------------------------------------------------------
 
-	while (sizeRow < 2) {
-		system("cls");
-		cout << "Row: ";
-		cin >> sizeRow;
-	}
-	while (sizeCol < 2) {
-		system("cls");
-		cout << "Col: ";
-		cin >> sizeCol;
+	//char simvol;
+	//int sizeRow = 0, sizeCol = 0, color;
+
+	//cout << "simvol: ";
+	//cin >> simvol;
+
+	//while (sizeRow < 2) {
+	//	system("cls");
+	//	cout << "Row: ";
+	//	cin >> sizeRow;
+	//}
+	//while (sizeCol < 2) {
+	//	system("cls");
+	//	cout << "Col: ";
+	//	cin >> sizeCol;
+	//}
+
+	//system("cls");
+	//cout << "Color(1 - Red, 2 - Green, 3 - Yellow): ";
+	//cin >> color;
+	//system("cls");
+
+	//if (color == 1) {
+	//	cout << "\033[31m";
+	//}
+	//else if (color == 2) {
+	//	cout << "\033[32m";
+	//}
+	//else if (color == 3) {
+	//	cout << "\033[33m";
+	//}
+
+	//for (int i = 0; i < sizeCol; i++) {
+	//	for (int j = 0; j < sizeRow; j++) {
+	//		if (i == 0 || i == sizeCol-1) {
+	//			cout << simvol;
+	//		}
+	//		else if (j == 0 || j == sizeRow-1) {
+	//			cout << simvol;
+	//		}
+	//		else {
+	//			cout << " ";
+	//		}
+	//	}
+	//	cout << endl;
+	//}
+	//cout << "\033[0m";
+
+//------------------------------------------------------------------}
+
+//{------------------------------------------------------------------
+
+	
+	const int size = 5;
+	int mas[size];
+	int Max;
+
+	for (int i = 0; i < size; i++) {
+		mas[i] = rand() % 100;
+		cout << mas[i] << " ";
 	}
 
-	system("cls");
-	cout << "Color(1 - Red, 2 - Green, 3 - Yellow): ";
-	cin >> color;
-	system("cls");
+	Max = mas[1];
 
-	if (color == 1) {
-		cout << "\033[31m";
-	}
-	else if (color == 2) {
-		cout << "\033[32m";
-	}
-	else if (color == 3) {
-		cout << "\033[33m";
-	}
-
-	for (int i = 0; i < sizeCol; i++) {
-		for (int j = 0; j < sizeRow; j++) {
-			if (i == 0 || i == sizeCol-1) {
-				cout << simvol;
-			}
-			else if (j == 0 || j == sizeRow-1) {
-				cout << simvol;
-			}
-			else {
-				cout << " ";
-			}
+	for (int i = 0; i < size; i++) {
+		if (mas[i] > Max) {
+			Max = mas[i];
 		}
-		cout << endl;
 	}
-	cout << "\033[0m";
 
+	cout << endl << Max;
 
+	
+
+//------------------------------------------------------------------}
 
 }
